2481430646e9053a8a20b7b64f19ac01641161e3
markysoft <markharrop@yahoo.com>
2025-01-19 13:21:13 +0000

    add assets to check for real file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import fs from 'fs';
 export async function handler(req: Request, res: ResponseBuilder) {
     console.log(req);
 
-    fs.readFileSync('/no-such-file', 'utf8');
+    fs.readFileSync('./test.txt', 'utf8');
     res.send("hello universe");
 }
