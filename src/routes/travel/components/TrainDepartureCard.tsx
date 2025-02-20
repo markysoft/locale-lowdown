fe0e5d6011f0584ee735a96f516d3bdd1e8698c2
markysoft <markharrop@yahoo.com>
2025-02-20 12:53:11 +0000

    ui tweaks

--- src/routes/travel/components/TrainDepartureCard.tsx
+++ src/routes/travel/components/TrainDepartureCard.tsx
@@ -1,23 +1,24 @@
 import { FC } from 'hono/jsx'
 import { Departure } from '../services/trainTimes'
 
-export const TrainDepartureCard: FC<{ service: Departure }> = (props: { service: Departure }) => {
+export const TrainDepartureCard: FC<{ service: Departure, showPlatforms: boolean }> =
+    (props: { service: Departure, showPlatforms: boolean }) => {
 
-    return (
-        <nav class="level" style={{ 'margin-bottom': '1.5em' }}>
-            <div class="level-left">
-                <div class="level-item">
-                    <p class="subtitle is-6"><strong>{props.service.standardTime}</strong> to {props.service.destination}</p>
+        return (
+            <nav class="level" style={{ 'margin-bottom': '1.5em' }}>
+                <div class="level-left">
+                    <div class="level-item has-text-centered has-text-left-desktop">
+                        <p class="subtitle is-6"><strong>{props.service.standardTime}</strong> to {props.service.destination} { props.showPlatforms ? `(platform ${props.service.platform})` : ''}</p>
+                    </div>
                 </div>
-            </div>
-            <div class="level-right">
-                <p class="level-item">
-                    {props.service.isCancelled
-                        ? <strong>Cancelled</strong>
-                        : <span>{props.service.expected === 'On time' ? props.service.expected : <strong>Expected {props.service.expected}</strong>}</span>
-                    }
-                </p>
-            </div>
-        </nav>
-    )
-}
+                <div class="level-right">
+                    <p class="level-item">
+                        {props.service.isCancelled
+                            ? <strong>Cancelled</strong>
+                            : <span>{props.service.expected === 'On time' ? props.service.expected : <strong>Expected {props.service.expected}</strong>}</span>
+                        }
+                    </p>
+                </div>
+            </nav>
+        )
+    }
--- src/routes/travel/components/TrainDeparturesList.tsx
+++ src/routes/travel/components/TrainDeparturesList.tsx
@@ -2,49 +2,57 @@ import { FC } from 'hono/jsx'
 import { Departure, Departures } from '../services/trainTimes'
 import { TrainDepartureCard } from './TrainDepartureCard'
 
+
+
 export const TrainDeparturesList: FC<{ departures: Departures }> = (props: { departures: Departures }) => {
+
+    const isActive = (crs: string) => {
+        return props.departures.crs === crs ? 'is-active' : ''
+    }
+
+    const showPlatforms = (): boolean => {
+        return props.departures.crs === 'SCA'
+    }
+
     return (
-        <>
-
-            <div class="card">
-                <header class="card-header">
-                    <div class="tabs">
-                        <ul style={{ 'margin-inline-start': '0em' }}>
-                            <li class="is-active">
-                                <a
-                                    aria-label="get Malton train times"
-                                    hx-get="/travel/train/mlt"
-                                    hx-target="#train-departures"
-                                    hx-indicator="#train-spinner">
-                                    Malton
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    aria-label="get Scarborough train times"
-                                    hx-get="/travel/train/sca"
-                                    hx-target="#train-departures"
-                                    hx-indicator="#train-spinner">
-                                    Scarborough
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </header>
-                <div class="card-content">
-
-                    <p class="content">Last updated: <strong>{props.departures.generatedAt}</strong> <span id="train-spinner" class="htmx-indicator">&nbsp;<i class="fa fa-spinner fa-spin"></i></span></p>
-                    {
-                        props.departures.trainServices.map((service: Departure, index: number) => {
-                            return <>
-                                <TrainDepartureCard service={service} />
-                                {index < props.departures.trainServices.length - 1 && <hr />}
-                            </>
-                        })
-                    }
+        <div class="card">
+            <header class="card-header">
+                <div class="tabs">
+                    <ul style={{ 'margin-inline-start': '0em' }}>
+                        <li class={isActive('MLT')}>
+                            <a
+                                aria-label="get Malton train times"
+                                hx-get="/travel/train/mlt"
+                                hx-target="#train-departures"
+                                hx-indicator="#train-spinner">
+                                Malton
+                            </a>
+                        </li>
+                        <li class={isActive('SCA')}>
+                            <a
+                                aria-label="get Scarborough train times"
+                                hx-get="/travel/train/sca"
+                                hx-target="#train-departures"
+                                hx-indicator="#train-spinner">
+                                Scarborough
+                            </a>
+                        </li>
+                    </ul>
                 </div>
+
+            </header>
+            <div class="card-content">
+
+                <p class="content">Last updated: <strong>{props.departures.generatedAt}</strong> <span id="train-spinner" class="htmx-indicator">&nbsp;<i class="fa fa-spinner fa-spin"></i></span></p>
+                {
+                    props.departures.trainServices.map((service: Departure, index: number) => {
+                        return <>
+                            <TrainDepartureCard service={service} showPlatforms={showPlatforms()} />
+                            {index < props.departures.trainServices.length - 1 && <hr />}
+                        </>
+                    })
+                }
             </div>
-        </>
+        </div>
     )
 }
