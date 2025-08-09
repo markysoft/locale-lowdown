151c9b99d9ad44288bf6276612286c65a7251637
Mark Harrop <markharrop@yahoo.com>
2025-08-09 18:38:29 +0100

    improve reads

--- src/routes/travel/components/NextBusCard.tsx
+++ src/routes/travel/components/NextBusCard.tsx
@@ -1,7 +1,4 @@
-import { FC } from 'hono/jsx'
-
-export const NextBusCard: FC<{ nextBusTo: string, nextBusFrom: string }> = (props: { nextBusTo: string, nextBusFrom: string }) => {
-
+export function NextBusCard({ nextBusTo, nextBusFrom }: { nextBusTo: string, nextBusFrom: string, }) {
     return (
         <div id='travel-bus'>
             <h2 class="title has-text-primary-15">Bus Departures</h2>
@@ -9,8 +6,8 @@ export const NextBusCard: FC<{ nextBusTo: string, nextBusFrom: string }> = (prop
                 <div class="card-content">
                     <div class="content has-text-centered">
                         <p>
-                            Next Bus to Malton is <strong>{props.nextBusTo}</strong> <br />
-                            Next Bus from Malton is <strong>{props.nextBusFrom}</strong>
+                            Next Bus to Malton is <strong>{nextBusTo}</strong> <br />
+                            Next Bus from Malton is <strong>{nextBusFrom}</strong>
                         </p>
                         <p>Check the <a href="https://getdown.org.uk/bus/bus/194.shtml" target="_blank">schedule</a> for more
                             details</p>
--- src/routes/travel/components/TrainDepartureCard.tsx
+++ src/routes/travel/components/TrainDepartureCard.tsx
@@ -1,24 +1,21 @@
-import { FC } from 'hono/jsx'
 import { Departure } from '../services/trainTimes'
 
-export const TrainDepartureCard: FC<{ service: Departure, showPlatforms: boolean }> =
-    (props: { service: Departure, showPlatforms: boolean }) => {
-
-        return (
-            <nav class="level" style={{ 'margin-bottom': '1.5em' }}>
-                <div class="level-left">
-                    <div class="level-item has-text-centered has-text-left-desktop">
-                        <p class="subtitle is-6"><strong>{props.service.standardTime}</strong> to {props.service.destination} { props.showPlatforms ? `(platform ${props.service.platform})` : ''}</p>
-                    </div>
+export function TrainDepartureCard({ service, showPlatforms }: { service: Departure, showPlatforms: boolean }) {
+    return (
+        <nav class="level" style={{ 'margin-bottom': '1.5em' }}>
+            <div class="level-left">
+                <div class="level-item has-text-centered has-text-left-desktop">
+                    <p class="subtitle is-6"><strong>{service.standardTime}</strong> to {service.destination} { showPlatforms ? `(platform ${service.platform})` : ''}</p>
                 </div>
-                <div class="level-right">
-                    <p class="level-item">
-                        {props.service.isCancelled
-                            ? <strong>Cancelled</strong>
-                            : <span>{props.service.expected === 'On time' ? props.service.expected : <strong>Expected {props.service.expected}</strong>}</span>
-                        }
-                    </p>
-                </div>
-            </nav>
-        )
-    }
+            </div>
+            <div class="level-right">
+                <p class="level-item">
+                    {service.isCancelled
+                        ? <strong>Cancelled</strong>
+                        : <span>{service.expected === 'On time' ? service.expected : <strong>Expected {service.expected}</strong>}</span>
+                    }
+                </p>
+            </div>
+        </nav>
+    )
+}
